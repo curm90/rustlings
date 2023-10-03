1a2849d6dc4846279f278f5f3926cc13c4c1e00f
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2023-10-03 17:38:46 +0100

    Solution: quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,7 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
+#[derive(Debug)]
 pub enum Command {
     Uppercase,
     Trim,
@@ -31,12 +30,15 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(n) => output.push(format!("{}{}", string, "bar".repeat(*n))),
+            }
+            println!("{:?}", output);
         }
         output
     }
@@ -44,18 +46,19 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
     fn it_works() {
-        let output = transformer(vec![
-            ("hello".into(), Command::Uppercase),
-            (" all roads lead to rome! ".into(), Command::Trim),
-            ("foo".into(), Command::Append(1)),
-            ("bar".into(), Command::Append(5)),
-        ]);
+        let output = transformer(
+            vec![
+                ("hello".into(), Command::Uppercase),
+                (" all roads lead to rome! ".into(), Command::Trim),
+                ("foo".into(), Command::Append(1)),
+                ("bar".into(), Command::Append(5))
+            ]
+        );
         assert_eq!(output[0], "HELLO");
         assert_eq!(output[1], "all roads lead to rome!");
         assert_eq!(output[2], "foobar");
